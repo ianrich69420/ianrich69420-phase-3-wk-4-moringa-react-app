ef1764e6b822d2fa8520f503f6142eebbc44c32e
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-08 10:56:40 +0300

    Initializing Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Login from  './components/Login';
 
 function App() {
+  const userDatabase = 'http://localhost:9292/users'
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(userDatabase)
+    .then(r => r.json())
+    .then(users => setUsers(users))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Login users={users} />
+  )
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+function Login({ users }) {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const errors = {
+        username: "Invalid username",
+        password: "Invalid password"
+    };
+
+    var { username, password } = document.forms[0];
+
+    const userData = users.find((user) => user.username === username.value);
+
+    if (userData) {
+      if (userData.password !== password.value) {
+        setErrorMessages({ name: "password", message: errors.password });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "username", message: errors.username });
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="username" required />
+          {renderErrorMessage("username")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="password" required />
+          {renderErrorMessage("password")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
