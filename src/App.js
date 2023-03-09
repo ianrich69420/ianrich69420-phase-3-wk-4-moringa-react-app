440ef03e4fd36145a799cd74a2d6b22cecb5b4bd
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-09 05:19:40 +0300

    Initializing Project Member Adder

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Login from  './components/Login';
 import Register from './components/Register';
 import Projects from './components/Projects';
 import AddProject from './components/AddProject'
+import AddProjectMember from './components/AddProjectMember';
 
 function App() {
   const [users, setUsers] = useState([])
   const [projects, setProjects] = useState([])
+  const [projectMembers, setProjectMembers] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:9292/users')
@@ -21,6 +23,12 @@ function App() {
       .then(projects => setProjects(projects))
   }, []);
 
+  useEffect(() => {
+    fetch('http://localhost:9292/project_members')
+      .then(r => r.json())
+      .then(projectMembers => setProjectMembers(projectMembers))
+  }, []);
+
   function deleteProject(projectToDelete) {
     fetch(`http://localhost:9292/projects/${projectToDelete.id}`, {
       method: "DELETE",
@@ -30,7 +38,7 @@ function App() {
 
   return (
     <div>
-      <AddProject projects={projects}/>
+      <AddProjectMember projectMembers={projectMembers} users={users} projects={projects} />
     </div>
   )
 }
---
+++ src/components/AddProjectMember.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+function AddProjectMember({ projectMembers, users, projects }) {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const errors = {
+        email: "Member email already used for project",
+        project_member: "User or Project does not exist"
+    };
+
+    var { membername, email, project_name } = document.forms[0];
+
+    const projectMemberObj = {
+        membername: membername.value,
+        email: email.value,
+        project_name: project_name.value
+    }
+
+    const projectData = projects.find((project) => project.project_name === project_name.value)
+
+    const userData = users.find((user) => user.email === email.value)
+
+    const projectMemberData = projectMembers.find((projectMember) => projectMember.project_id === projectData.id && projectMember.email === email.value );
+
+    console.log(projectMemberData)
+    console.log(userData)
+    console.log(projectData)
+    console.log(users)
+
+    if (userData && projectData && !projectMemberData) {
+      fetch("http://localhost:9292/project_members", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(projectMemberObj)
+          })
+          .then(r => r.json())
+          .then(data => console.log(data));
+      setIsSubmitted(true);
+    } else if (projectMemberData) {
+      setErrorMessages({name: "email", message: errors.email})
+    } else {
+      setErrorMessages({name: "project_member", message: errors.project_member})
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+  name === errorMessages.name && (
+    <div className="error">{errorMessages.message}</div>
+  );
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Membername </label>
+          <input type="text" name="membername" required />
+        </div>
+        <div className="input-container">
+          <label>Email </label>
+          <input type="email" name="email" required />
+          {renderErrorMessage("email")}
+        </div>
+        <div className="input-container">
+          <label>Project Name </label>
+          <input type="text" name="project_name" required />
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+          {renderErrorMessage("project_member")}
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="project-member-form">
+        <div className="title">Add Project Member</div>
+        {isSubmitted ? <div>Member added!</div> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default AddProjectMember;
